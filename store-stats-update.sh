5b7e5bc1e51bbce734039721b32ec70710f5f081
Alex Rutgers <aprutgers@gmail.com>
2023-07-21 14:35:16 +0200

    added

---
+++ store-stats-update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+date=`date '+%y%m%d'`
+echo "`date`: creating /home/ec2-user/site-test/stats/stats.$date"
+/home/ec2-user/site-test/collect_stats.sh>/home/ec2-user/site-test/stats/stats.$date
+echo "`date`: done"
