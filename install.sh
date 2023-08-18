73ef328f444f5453b756f18f2140e8c9ed0dfeda
Alex Rutgers <aprutgers@gmail.com>
2023-08-18 11:24:12 +0200

    added-tmp-ramdisk.service

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo yum -y install docker
 sudo yum -y install bc
 gem install selenium-webdriver -v 4.1.0
 gem install rspec
+cp /home/ec2-user/site-test/tmp-ramdisk.service /usr/lib/systemd/system
