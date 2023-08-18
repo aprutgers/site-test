90eb90e9982224495c0326b8c4288b55b5596a96
Alex Rutgers <aprutgers@gmail.com>
2023-08-18 18:49:28 +0200

    updated

--- runner.rb
+++ runner.rb
@@ -10,9 +10,7 @@ $country  = ""
 $vrecurse = 0
 $ctr      = 10 # safe default
 $debug    = 1  # default
-#$free_mem = 1023 # safe edge before trashing
-#$free_mem = 512 # sharp edge before trashing
-$free_mem = 256 # very sharp edge before trashing
+$free_mem = 150
 
 def log(str)
   ts=Time.now
