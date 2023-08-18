dea8ec366df815ee9b10b0da4391811e4ab75f70
Alex Rutgers <aprutgers@gmail.com>
2023-08-18 11:28:59 +0200

    updated

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+#!/bin/sh
+
+# required software Ruby, Docker, bc, and gem selenium-webdriver + rspec
 sudo yum -y install ruby
 sudo yum -y install docker
 sudo yum -y install bc
 gem install selenium-webdriver -v 4.1.0
 gem install rspec
-cp /home/ec2-user/site-test/tmp-ramdisk.service /usr/lib/systemd/system
+
+# systemd 
+sudo cp /home/ec2-user/site-test/tmp-ramdisk.service /usr/lib/systemd/system
+sudo systemctl enable tmp-ramdisk.service
+sudo systemctl start tmp-ramdisk.service
+
+sudo cp /home/ec2-user/site-test/sitetest.service /etc/systemd/system
+sudo systemctl enable sitetest.service
+#sudo systemctl sitetest.service
