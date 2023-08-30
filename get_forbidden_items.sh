e5f4f3d1b220742ce7c9ab92894084c962718408
Alex Rutgers <aprutgers@gmail.com>
2023-08-30 14:56:58 +0200

    fix

--- get_forbidden_items.sh
+++ get_forbidden_items.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-grep -B +2 Forbidden /mnt/tmp/test-runner-instance*.log|grep 2022|awk -F/ '{ print $10 }'
+grep -B +2 Forbidden /nvme/tmp/test-runner-instance*.log|grep 2022|awk -F/ '{ print $10 }'
